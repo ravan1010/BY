05ab6124f7e5203519c01597cdf5ceb36ec5072f
ravan1010 <ravanravana177@gmail.com>
2026-04-26 11:54:53 +0530

    main

--- vendor/src/api.js
+++ vendor/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL : "https://api.byslot.online"
+  baseURL : "https://api.byslot.online",
   withCredentials: true, // 🔥 important for cookies
 });
 
